562ecc1fa41bc085e01e3221744262bc53ab47f3
poloma <873800030@qq.com>
2021-08-24 23:17:57 +0800

    feat: 提交HTTP

--- Node/Even-Loop/event-loop.js
+++ Node/Even-Loop/event-loop.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 async function async3() {
     console.log('async 3');
 }
@@ -9,17 +11,24 @@ async function async1() {
     // new Promise((resolve) => {
     //      console.log('async2')
     // }).then(() => {
-    //      console.log('async1 end')
+    //      new Promise((resolve) => {
+    //          console.log('async');
+    //      }).then(() => {
+    //          console.log('async1 end')
+    //      })
     // })
     console.log('async1 end'); // 微任务队列 添加到 微任务队列里面 1
 }
-
 
 async function async2() {
     console.log('async2');
 }
 
 console.log('script start');
+
+fs.readFile(__filename, () => {
+    console.log('readFile'); // 读写流 在最后执行
+})
 
 // 宏任务
 setTimeout(function () {
