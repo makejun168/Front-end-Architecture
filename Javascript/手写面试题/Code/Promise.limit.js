89411d529897880a48889fb4b2eacd6fcf5a3818
poloma <873800030@qq.com>
2021-12-20 22:50:51 +0800

    feat: PromiseLimit方法实现

--- Javascript/手写面试题/Code/Promise.limit.js
+++ Javascript/手写面试题/Code/Promise.limit.js
@@ -20,5 +20,45 @@ class PromiseQueue {
     constructor(options ={}) {
         this.concurrency = options.concurrency || 1;
         this.currentCount = 0;
+        // 等待执行的 Promise 队
+        this.pendingList = [];
+    }
+
+    /**
+     * 加载到等待队列里面
+     * @param task
+     */
+    add(task) {
+        this.pendingList.push(task);
+        this.run() // 执行
+    }
+
+    /**
+     * 执行核心逻辑
+     */
+    run() {
+        if (this.pendingList.length === 0){
+            return;
+        }
+
+        if (this.currentCount === this.concurrency) {
+            return;
+        }
+
+        // 执行开始 +
+        this.currentCount++;
+
+        // 拿出来 第一个
+        const { fn } = this.pendingList.sort((a,b) => b.priority - a.priority).shift();
+        const promise = fn();
+        promise.then(this.completeOne.bind(this)).catch(this.completeOne.bind(this))
+    }
+
+    /**
+     * 完成
+     */
+    completeOne() {
+        this.currentCount--;
+        this.run();
     }
 }
